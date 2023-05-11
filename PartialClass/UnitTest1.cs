b9c5324de57e02fec8e2ac718ec5bcf99f6ed6af
Arpita Terdal <arpitaterdal@gmail.com>
2023-05-11 13:05:47 +0530

    changes to unit test

--- PartialClass/UnitTest1.cs
+++ PartialClass/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace PartialClass
             var person = new Person("John", new DateTime(1998, 2, 26));
             Assert.AreEqual("Hi, my name is John and I'm 30 years old", person.Introduce());
             Assert.AreEqual("John", person.Name);
+            Console.WriteLine(person.Name);
             Assert.AreEqual(new DateTime(1992, 2, 26), person.BirthDate);
         }
     }
